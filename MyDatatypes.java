939d36fb02d2f401df125c372a94f7650c3ef119
AshishKumawat04 <akmarwal603@gmail.com>
2026-02-14 15:59:55 +0530

    this is my commit

---
+++ MyDatatypes.java
@@ -0,0 +1,26 @@
+public class MyDatatypes{
+    public static void main(String[] args){
+        int theTestValue = 58;
+        theTestValue = theTestValue + 20;
+        String firstName = "Ashish";
+        int theSecondTestValue = 55;
+        long theThirdTestValue = 587;
+        short theFourthTestValue = 38;
+        byte theFifthTestValue = 11;
+
+
+              //djdkjd
+        float theSixthTestValue = 85.85f;
+        double theSeventhTestValue = 57.8578758;
+
+        char theEighthTestValue = '@';
+        boolean theTrueFalse = 87>85;
+
+        double x = theSixthTestValue + theSeventhTestValue;
+
+        System.out.println("The plus of the two decimal values is : "+x);
+        
+        System.out.println("Now the value 87>85 : "+theTrueFalse);
+         
+        }
+}
