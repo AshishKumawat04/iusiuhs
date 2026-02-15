842a645b30f51522be730c1c9a10447654773d26
AshishKumawat04 <akmarwal603@gmail.com>
2026-02-15 07:31:45 +0530

    this is my third commit

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        int myAge = 18;
+
+        myAge = myAge + 2;
+        System.out.println(myAge);
+        //this is test for git
+    }
+}
